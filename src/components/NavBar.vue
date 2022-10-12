16b62c7e985790d076fc7307a0e963a1941ccd31
danbirle <razvan.birle@gmail.com>
2022-10-12 14:39:27 +0200

    mobile first nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -24,10 +24,44 @@
         <option>dscending</option>
       </select>
     </div>
-    <div>
+    <div class="input-wrapper">
       <label for="filter-input">Filter: </label>
       <input id="filter-input" type="text" />
     </div>
     <button>&gt;</button>
   </header>
 </template>
+
+<style scoped lang="scss">
+header {
+  border-bottom: 5px solid rgb(2, 0, 36);
+  gap: 1rem;
+  align-items: center;
+  display: grid;
+  grid-template-rows: auto auto;
+  grid-template-columns: repeat(4, auto);
+  justify-items: space-between;
+  width: 100%;
+  background: #181818;
+  padding: .5rem 1rem;
+  
+  & > button {
+    grid-row: span 2;
+    height: 100%;
+    padding: 0 .5rem;
+  }
+
+  .input-wrapper {
+    display: flex;
+    gap: .5rem;
+    grid-row: 2;
+    grid-column: 2 / span 2;
+
+    input {
+      flex-shrink: 2;
+      width: 100%;
+      max-width: 100%;
+    }
+  }
+}
+</style>
