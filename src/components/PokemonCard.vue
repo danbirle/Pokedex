c02b55f1bb5625a461562417f359e16433a45385
danbirle <razvan.birle@gmail.com>
2022-10-12 14:45:20 +0200

    markup pokemon card

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: String,
+  weight: Number,
+  height: Number,
+  abilities: Array,
+  sprite: String,
+})
+</script>
+
+<template>
+  <div class="wrapper">
+    <div>
+      <div>{{ props.name }}</div>
+      <div><span>Weight: </span><br />{{ props.weight }}hg</div>
+      <div><span>Height: </span><br />{{ props.height }}dm</div>
+      <div class="abilities">
+        <div v-for="ability in props.abilities" v-bind:key="`pokemon_ability_${ability}`">
+          {{ ability }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
